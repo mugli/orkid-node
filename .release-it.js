5c1536cdf3c7c80f760cc25acde4acae950e1cf1
Mehdi Hasan <mhasan@omicronlab.com>
2019-08-13 19:41:12 +0600

    Setup automated release

---
+++ .release-it.js
@@ -0,0 +1,31 @@
+module.exports = {
+  hooks: {
+    'before:release': 'npx auto-changelog -p'
+  },
+  git: {
+    changelog: 'npx auto-changelog --stdout --commit-limit false --unreleased --template ./changelog-template.hbs',
+    requireCleanWorkingDir: true,
+    requireUpstream: true,
+    requireCommits: true,
+    addUntrackedFiles: false,
+    commit: true,
+    commitMessage: 'Release ${version}',
+    commitArgs: '',
+    tag: true,
+    tagName: '${version}',
+    tagAnnotation: 'Release ${version}',
+    tagArgs: '',
+    push: true,
+    pushArgs: '--follow-tags'
+  },
+  npm: {
+    publish: false
+  },
+  github: {
+    release: true,
+    releaseName: 'Release ${version}',
+    preRelease: false,
+    draft: false,
+    tokenRef: 'GITHUB_TOKEN'
+  }
+};
